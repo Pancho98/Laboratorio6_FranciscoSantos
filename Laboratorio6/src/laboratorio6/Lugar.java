33eb6db49edec8abcc7d23763c0166ae01660312
Francisco Santos <fjgsantos98@gmail.com>
2017-08-25 13:26:45 -0600

    Gui y clase lugar

---
+++ Laboratorio6/src/laboratorio6/Lugar.java
@@ -0,0 +1,11 @@
+
+package laboratorio6;
+
+
+public class Lugar {
+    private String Nombre;
+    private String Direccion;
+    private int Seguridad;
+    private int entrada;
+    private int salida;
+}
